0b2290b1262d5f437a842e30f6dfd805cdbcf881
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:29:01 +0000

    chore(release): v3.0.2

--- archive/dataarchivefile/dataarchivefile_DataArchiveFile.go
+++ archive/dataarchivefile/dataarchivefile_DataArchiveFile.go
@@ -2,10 +2,10 @@ package dataarchivefile
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-archive-go/archive/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-archive-go/archive/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-archive-go/archive/v3/dataarchivefile/internal"
+	"github.com/cdktf/cdktf-provider-archive-go/archive/v3/dataarchivefile/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- archive/file/file_File.go
+++ archive/file/file_File.go
@@ -2,10 +2,10 @@ package file
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-archive-go/archive/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-archive-go/archive/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-archive-go/archive/v3/file/internal"
+	"github.com/cdktf/cdktf-provider-archive-go/archive/v3/file/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 

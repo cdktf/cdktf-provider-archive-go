7afe39e65bf46753a9a565e539e9527f1ab1ae80
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-12-06 04:06:08 +0000

    chore(release): v10.3.0

--- archive/provider/ArchiveProvider.go
+++ archive/provider/ArchiveProvider.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
-// Represents a {@link https://registry.terraform.io/providers/hashicorp/archive/2.6.0/docs archive}.
+// Represents a {@link https://registry.terraform.io/providers/hashicorp/archive/2.7.0/docs archive}.
 type ArchiveProvider interface {
 	cdktf.TerraformProvider
 	Alias() *string
@@ -186,7 +186,7 @@ func (j *jsiiProxy_ArchiveProvider) TerraformResourceType() *string {
 }
 
 
-// Create a new {@link https://registry.terraform.io/providers/hashicorp/archive/2.6.0/docs archive} Resource.
+// Create a new {@link https://registry.terraform.io/providers/hashicorp/archive/2.7.0/docs archive} Resource.
 func NewArchiveProvider(scope constructs.Construct, id *string, config *ArchiveProviderConfig) ArchiveProvider {
 	_init_.Initialize()
 
@@ -204,7 +204,7 @@ func NewArchiveProvider(scope constructs.Construct, id *string, config *ArchiveP
 	return &j
 }
 
-// Create a new {@link https://registry.terraform.io/providers/hashicorp/archive/2.6.0/docs archive} Resource.
+// Create a new {@link https://registry.terraform.io/providers/hashicorp/archive/2.7.0/docs archive} Resource.
 func NewArchiveProvider_Override(a ArchiveProvider, scope constructs.Construct, id *string, config *ArchiveProviderConfig) {
 	_init_.Initialize()
 
--- archive/provider/README.md
+++ archive/provider/README.md
@@ -1,3 +1,3 @@
 # `provider`
 
-Refer to the Terraform Registry for docs: [`archive`](https://registry.terraform.io/providers/hashicorp/archive/2.6.0/docs).
+Refer to the Terraform Registry for docs: [`archive`](https://registry.terraform.io/providers/hashicorp/archive/2.7.0/docs).
